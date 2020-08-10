3439471dbf8d043d6a600c2127842bea782271d4
AuditProjectMFRP <68848812+AuditProjectMFRP@users.noreply.github.com>
2020-08-10 15:03:37 +0530

    Update AuditBenchmarkTests.cs
    final commit

--- AuditBenchmarkTest/AuditBenchmarkTests.cs
+++ AuditBenchmarkTest/AuditBenchmarkTests.cs
@@ -13,39 +13,16 @@ namespace AuditBenchmarkTest
         public void GetAuditBenchmark_ReturnsListOfAuditBenchMark()
         {
 
-            //Act
-            AuditBenchmarkController obj = new AuditBenchmarkController();
-            var data = obj.GetAuditBenchmark();
-            var okResult = data.Result as OkObjectResult;
-
-            //Assert
-            Assert.AreEqual(200, okResult.StatusCode);
-
-
-        }
-
-        [Test]
-        public void GetAuditBenchmark_To_Count_NumberOfElements_ReturnsListOfAuditBenchMark()
-        {
-            //Arrange
-            List<AuditBenchmarkModel> benchmark = new List<AuditBenchmarkModel>
-                    {
-                       new AuditBenchmarkModel{AuditType="Internal",BenchmarkNoAnswers=3},
-                       new AuditBenchmarkModel{AuditType="SOX",BenchmarkNoAnswers=1}
-                    };
-
-            //Act
+              //Act
             AuditBenchmarkController obj = new AuditBenchmarkController();
             var data = obj.GetAuditBenchmark();
             var okResult = data.Result as OkObjectResult;
             int val = ((List<AuditBenchmarkModel>)okResult.Value).Count;
 
             //Assert
+            Assert.AreEqual(200, okResult.StatusCode);
             Assert.AreEqual(2, val);
-            
         }
-
-
 
 
 
